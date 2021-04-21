d13a4a9d2b8d774f5cdc2e96b84e74777ec43123
Gabriel Sailer <gabriel.sailer@riseup.net>
2021-04-21 14:01:56 +0200

    Base functionality webapp

--- webapp/script.js
+++ webapp/script.js
@@ -1,31 +1,36 @@
 const API_ENDPOINT = 'https://pngme.azurewebsites.net';
-const WSS_ENDPOINT = 'wss://pngme.azurewebsites.net/sessions/join'
+const WSS_ENDPOINT = 'wss://pngme.azurewebsites.net/sessions/join';
 
-var socket = null
+var socket = null;
+var me = null;
 
-function joinHandler() {
+async function joinHandler() {
+  const name = document.getElementById('join-name').value;
+  if (name === "") {
+    alert("Please supply a name");
+    return;
+  }
   document.getElementById('register').classList.add('hidden');
   document.getElementById('joining').classList.remove('hidden');
-
-  const name = document.getElementById('join-name').value;
-  // TODO: check if name === ""
-
-  socket = new WebSocket(`${WSS_ENDPOINT}/${createUUID()}?name=${name}`);
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  me = {"client_id": createUUID(), "name": name, "client_type": "pinger"}
+  socket = new WebSocket(`${WSS_ENDPOINT}/${me.client_id}?name=${me.name}`);
   socket.onmessage = function (event) {
+    var msg = null;
     try {
-      const msg = JSON.parse(event.data);
-      handleMessage(msg);
+      msg = JSON.parse(event.data);
     } catch {
       console.error("Invalid json from websocket.");
       return;
     }
+    handleMessage(msg);
   }
   
   fetch(`${API_ENDPOINT}/sessions/`).then(function (response) {
     return response.json();
   }).then(function (data) {
     console.log(data);
-    // data :: List[User]
+    // data :: List[UserView]
     for (var i = 0; i < data.length; i++) {
       addClientView(data[i]);
     }
@@ -36,26 +41,55 @@ function joinHandler() {
   document.getElementById('main').classList.remove('hidden');
 }
 
+function sendPing(user) {
+  console.log(`Sending ping to ${user.name}`);
+  msg = {
+    "mtype": "PING",
+    "sender": me.client_id,
+    "recipient": user.client_id
+  }
+  socket.send(JSON.stringify(msg));
+}
+
 function handleMessage(msg) {
   switch(msg.mtype) {
     case "JOIN":
+      if (msg.user.client_id == me.client_id) return;
       console.log(`${msg.user.name} joined.`);
       addClientView(msg.user);
       break;
     case "LEAVE":
+      if (msg.user.client_id == me.client_id) return;
       console.log(`${msg.user.name} left.`);
       rmClientView(msg.user);
+      break;
+    case "PING":
+      if (msg.sender == me.client_id) return;
+      break;
+    case "PONG":
+      if (msg.sender == me.client_id) return;
+      handlePong(msg);
       break;
     default:
       console.log("Unknown event:", msg);
   }
 }
 
+function handlePong(msg) {
+  if (msg.recipient === me.client_id) {
+    console.log(`Client ${msg.sender} reacted with ${msg.status}`);
+  } else {
+    console.log(`Client ${msg.sender} ponged`);
+  }
+}
+
 function addClientView(client) {
   if (!document.getElementById(client.client_id)) {
+    if (client.client_id === me.client_id) return;
     elem = document.createElement("div");
     elem.setAttribute("id", client.client_id);
     elem.innerHTML = `<h6>${client.name}</h6>`;
+    elem.onclick = function () {sendPing(client)};
     container = document.getElementsByClassName('ping-container')[0];
     container.appendChild(elem);
   }
@@ -69,6 +103,7 @@ function rmClientView(client) {
     console.error(`Client ${client.client_id} not found in document`);
   }
 }
+
 
 // https://gist.github.com/jsmithdev/1f31f9f3912d40f6b60bdc7e8098ee9f
 function createUUID(){
